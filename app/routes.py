e37987a713475e1792243ed5948b03b263b7cf6f
Brandon Wong <brandon.wong368@gmail.com>
2021-09-17 22:21:55 -0400

    Add CRUD endpoints

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,8 @@ import os
 from dotenv import load_dotenv
 import pymongo
 from pymongo import MongoClient
+from bson import json_util
+import json
 
 # connect to MongoDB
 load_dotenv()
@@ -18,15 +20,22 @@ def index():
     return "Hello, World!"
 
 # insert a document into the database
-@app.route('/add ' , methods=["POST"])
+@app.route('/add' , methods=["POST"])
 def add():
     body = request.get_json(force=True)
     image_collection.insert_one(body)
 
     return "Document uploaded successfully"
 
-
-@app.route("/delete_one" , methods = ["DELETE"])
+@app.route("/delete" , methods = ["DELETE"])
 def delete():
+    body = request.get_json(force=True)
+    image_collection.delete_one(body)
 
-    return
+    return "Document deleted successfully"
+
+@app.route("/search", methods = ["GET"])
+def search():
+    body = request.get_json(force=True)
+    document = image_collection.find_one(body)
+    return json.loads(json_util.dumps(document))
